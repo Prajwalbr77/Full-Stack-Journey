91eaaa4fd77ddd23116a786a753b32509b0d0826
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-06 19:22:47 +0530

    Add files via upload

---
+++ Javascript/console.js
@@ -0,0 +1,11 @@
+console.log("Before the code");
+let age = 13;
+if(age > 18){
+    console.log('You can drive..');
+    console.log('you can vote');
+}
+if(age < 18){
+    console.log('You cannot drive..');
+    console.log('you cannot vote');
+}
+console.log("before the code");
