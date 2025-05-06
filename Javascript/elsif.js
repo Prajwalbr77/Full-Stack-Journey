89cd2d35aaa4232e7bf7d732059823bca4f57a33
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-06 19:24:00 +0530

    Add files via upload

---
+++ Javascript/elsif.js
@@ -0,0 +1,10 @@
+let team ="csk";
+if(team === "rcb"){
+    console.log("Virat kholi is the captain!.");
+}
+else if(team ==="csk"){
+ console.log("Dhoni is the captain");
+}
+else if(team ==="mi"){
+    console.log("Hardhik is the captain")
+}
