b28daf323063aaddf852c46b0e04fbbe360c84ff
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-06 19:23:34 +0530

    Add files via upload

---
+++ Javascript/elsee.js
@@ -0,0 +1,12 @@
+let team ="rcb";
+if(team ==="pbks"){
+    console.log("This is an bengaluru team!");
+}else if(team ==="mi"){
+    console.log("This is an mumbai team!");
+}
+else if(team ==="csk"){
+    console.log("This is an chennai team!");
+}
+else{
+    console.log("This team is not present in ipl!");
+}
