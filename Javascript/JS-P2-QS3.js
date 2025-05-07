2705bd4d48359202493187b8d713bbbbf3a9d800
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-07 10:51:48 +0530

    Add files via upload

---
+++ Javascript/JS-P2-QS3.js
@@ -0,0 +1,12 @@
+let quatar = 1;
+switch(quatar){
+    case 1: console.log("January , Feb , march");
+    break;
+    case 2: console.log("April , may , June");
+    break;
+    case 3: console.log("July , aug , sep");
+    break;
+    case 4: console.log("october , Nov , Dec");
+    break;
+    default:console.log("Ivalid");
+}
