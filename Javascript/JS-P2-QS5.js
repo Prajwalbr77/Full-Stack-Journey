5e7e5e71a63569f65b5fce1f5e85cf6124c1503b
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-07 10:54:50 +0530

    Add files via upload

---
+++ Javascript/JS-P2-QS5.js
@@ -0,0 +1,14 @@
+let a =113;
+let b= 113;
+let c=80;
+if(a>b){
+    if(a>c){
+        console.log(a,"A Is the largest.");
+    }else{
+        console.log(c,"C is the largest!");
+    }
+}if(b>c){
+    console.log(b,"B is the largest!");
+}else{
+    console.log(c,"C is the largest!");
+}
