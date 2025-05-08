ea0f97567cce265dbe522193350bd018249a54e7
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-08 22:48:45 +0530

    Add files via upload

---
+++ Javascript/MonTue.js
@@ -0,0 +1,22 @@
+let days = prompt("Enter the day form 1 to 7:");
+console.log(days);
+let day = 6;
+switch(day){
+    case 1:console.log("Yeah its Monday!");
+    break;
+    case 2:console.log("Yeah its Tuesday!");
+    break;
+    case 3:console.log("Yeah its Wednesday!");
+    break;
+    case 4:console.log("Yeah its Thrusday!");
+    break;
+    case 5:console.log("Yeah its Friday!");
+    break;
+    case 6:console.log("Yeah its Saturday!");
+    break;
+    case 7:console.log("Yeah its Sunday!");
+    break;
+    default:console.log("Its not a valid day!")
+}
+
+
