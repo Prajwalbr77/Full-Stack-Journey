1f19ded4e092631751f3ea982cb79ea6c027aba3
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-08 22:49:48 +0530

    Add files via upload

---
+++ Javascript/PracticeQs1.js
@@ -0,0 +1,16 @@
+let size = "xl";
+if(size == "xl"){
+    console.log("Price is Rs:250");
+}
+else if(size=="l"){
+   console.log("Price is Rs:200");
+}
+else if(size=="m"){
+    console.log("Price is Rs:100");
+}
+else if(size=="s"){
+    console.log("Price is Rs:50");
+}
+else{
+    console.log("Invalid Price");
+}
