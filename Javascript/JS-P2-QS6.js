df4b5eebd7bc76cbfee794c8da11aca82c4c90ca
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-08 21:45:11 +0530

    Add files via upload

---
+++ Javascript/JS-P2-QS6.js
@@ -0,0 +1,7 @@
+let num1 = 677;
+let num2 = 377;
+if((num1%10)==(num2%10)){
+    console.log("Both the number is having the same digit.",num1%10);
+}else{
+    console.log("No the number doesnt contains any same last digit !");
+}
