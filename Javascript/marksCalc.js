ca8a216b2f07851969ece88579a9d1fd5ea42bdc
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-08 21:45:40 +0530

    Add files via upload

---
+++ Javascript/marksCalc.js
@@ -0,0 +1,13 @@
+let marks = 17;
+if(marks>=35){
+    console.log("Pass");
+    if(marks>=80){
+        console.log("O");
+    }
+    else{
+        console.log("A");
+    }
+}
+else{
+    console.log("Better Luck Next Time!");
+}
