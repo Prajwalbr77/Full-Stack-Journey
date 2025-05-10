9d5cb7bf597cfd8883b281f05d9357a18894d3ab
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-10 16:04:06 +0530

    Add files via upload

---
+++ Javascript/traficLigh.js
@@ -0,0 +1,10 @@
+let color ="yellow";
+if(color ==="red"){
+    console.log("Stop! The color is red");
+}
+if(color ==="yellow"){
+    console.log("Slow down! The color is yellow");
+}
+if(color ==="green"){
+    console.log("Go! The color is green");
+}
