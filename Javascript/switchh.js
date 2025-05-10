f822286624e5be37a2133614aaad8915fae90721
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-10 16:02:46 +0530

    Add files via upload

---
+++ Javascript/switchh.js
@@ -0,0 +1,13 @@
+let team = "RCB";
+switch(team){
+    case"RCB":console.log("It is the Bengaluru team!");
+    break;
+    case"MI":console.log("It is the Mumbai team!");
+    break;
+    case"DC":console.log("It is the Delhi team!");
+    break;
+    case"CSK":console.log("It is the Chennai team!");
+    break;
+    default:console.log("It is not a IPL team..!");
+
+}
