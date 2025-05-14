dccb95a8b027e8c588dfcbf7cc0e40ff66eb0c43
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-14 12:39:39 +0530

    Add files via upload

---
+++ Javascript-P2,3/aaa.js
@@ -0,0 +1,12 @@
+let team = Number(prompt("Please enter your team number:"));
+switch(team){
+    case 18: console.log("Royal Challenger Bengaluru!");
+    break;
+    case 7: console.log("Chennai Super Kings");
+    break;
+    case 45:console.log("Mumbai Indians!");
+    break;
+    case 67:console.log("Punjab kings");
+    break;
+    default:console.log("Not a valid team!");
+}
