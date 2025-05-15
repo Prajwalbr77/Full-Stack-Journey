766f1f7421934538fa92d488fe4518d4d5908b39
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-15 13:56:28 +0530

    Add files via upload

---
+++ Javascript-P2,3/StrngMthds.js
@@ -0,0 +1,9 @@
+// let name = "   Prajwal   ";
+// let newName = name.trim();
+// console.log(name.toLowerCase());
+
+
+// let wordss = "ILoveCricket";
+// console.log((wordss.strike.indexOf(4)));
+let price = 88;
+console.log("Your pencil price is: ",`${price}`);
