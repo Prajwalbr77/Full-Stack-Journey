6aeb8b89ad2e2d5f2c73138d096d245577405256
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-18 08:38:07 +0530

    Add files via upload

---
+++ Javascript-P4/LoopssWithArys.js
@@ -0,0 +1,9 @@
+let fruits =["apple","banana","Orange","Pineapple","StrawBerry","JackFruit"];
+for(let i=0;i<fruits.length;i++){
+    console.log(i,fruits[i]);
+}
+console.log("------------------------------------------")
+let fruitts =["apple","banana","Orange","Pineapple","StrawBerry","JackFruit"];
+for(let i=fruitts.length-1;i>=0;i--){
+    console.log(i,fruitts[i]);
+}
