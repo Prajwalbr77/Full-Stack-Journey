bf4dc208035b3915b86d43109343142b6d25492a
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-18 08:39:36 +0530

    Add files via upload

---
+++ Javascript-P4/Nestedlps.js
@@ -0,0 +1,7 @@
+let heros =[["ironman","spiderman","thor"],["superman","wonder women","flash"]];
+for(let i=0;i<heros.length;i++){
+    console.log(`List #${i}`);
+    for(let j=0;j<heros[i].length;j++){
+        console.log(heros[i][j]);
+    }
+}
