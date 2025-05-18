7324d00a220dd69c494085492b3edb925003f225
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-18 08:37:20 +0530

    Add files via upload

---
+++ Javascript-P4/FavMovie.js
@@ -0,0 +1,10 @@
+const mov = "LCD";
+let gues = prompt("Try your luck :");
+while((mov!=gues)&&(gues!="quit")){
+    gues = prompt("Opp's Try again!");
+}
+if(mov==gues){
+    console.log("Congratulations!");
+}else{
+    console.log("Your done!");
+}
