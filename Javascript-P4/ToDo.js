d584ccbde4fb4f17fd4b8b1b789092e5824ddefd
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-19 08:32:15 +0530

    Add files via upload

---
+++ Javascript-P4/ToDo.js
@@ -0,0 +1,27 @@
+let todo =[];
+let req =prompt("Enter the request :");
+while(true){
+    if(req =="quit"){
+        console.log("Your are quiting the App!");
+        break;
+    }
+    if(req =="list"){
+        console.log("-----------");
+        for(let i=0;i<todo.length;i++){
+            console.log(i,todo[i]);
+        }
+        console.log("------------");
+    }
+    else if(req == "add"){
+       let task =prompt("Please enter your task to add!");
+       todo.push(task);
+       console.log("Task is added !");
+    }else if(req == "delete"){
+        let idx = prompt("Please enter the task index");
+        todo.splice(idx,1);
+        console.log("Task is deleted !");
+    } else{
+        console.log("Sorry you request is invalid! ");
+    }
+    req = prompt("Please enter your request:");
+}
