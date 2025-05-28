17bbbfd10c0ec3371bf5a835206afb408917eed5
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-05-28 21:59:43 +0530

    Update Arrayss.js

--- Javascript-P2,3/Arrayss.js
+++ Javascript-P2,3/Arrayss.js
@@ -2,4 +2,3 @@ let items =["smartphone","book","pen","scale","eraser"];
 console.log(items.indexOf("smartphone"));
 console.log(items.indexOf("smartphone"));
 console.log(items.indexOf("smartphone"));
-console.log(items.indexOf("smartphone"));
