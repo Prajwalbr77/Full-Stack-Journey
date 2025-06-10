2a739cc75e9a9aed3a5fa69dfeba7baf04df90b8
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-06-10 10:21:11 +0530

    Update JS-P2-QS6.js

--- Javascript/JS-P2-QS6.js
+++ Javascript/JS-P2-QS6.js
@@ -4,4 +4,6 @@ if((num1%10)==(num2%10)){
     console.log("Both the number is having the same digit.",num1%10);
 }else{
     console.log("No the number doesnt contains any same last digit !");
+
+    console.log("Happy");
 }
