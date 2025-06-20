41e27664eaa003b0d97122f8ab58d2aa8b4641d3
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-06-20 10:10:06 +0530

    Add files via upload

---
+++ Javascript-P5/GuessNmbr.js
@@ -0,0 +1,19 @@
+const max =prompt("Enter the max value!");
+const random = Math.floor(Math.random()*max)+1;
+let guess = prompt("Guess the number!");
+while(true){
+    if(guess =="quit"){
+        console.log("You quit!");
+        break;
+    }
+    if(guess == random){
+        console.log("Congratulations you got it",random);
+        break;
+    }
+    else if(guess < random){
+        guess =prompt("Your guess is small");
+    }
+    else{
+        guess = prompt("Your guess is large !");
+    }
+}
