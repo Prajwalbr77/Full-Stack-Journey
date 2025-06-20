e4bfcf8991db9ea62a6919f4b795bf6e00a3c416
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-06-20 09:09:27 +0530

    Add files via upload

---
+++ Javascript-P5/ArrayOfObjts.js
@@ -0,0 +1,14 @@
+const student = [
+    {
+      name :"Prajwal",
+      rank :7,
+    },
+    {
+      name :"Shravya",
+      rank :7,
+    },
+    {
+      name :"Adithya",
+      rank :5
+    }
+];
