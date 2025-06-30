83639383570d3570366efddb8d839f5ab15fbe50
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-06-30 12:32:37 +0530

    Add files via upload

---
+++ Javascript-P6/AssignQs1.js
@@ -0,0 +1,23 @@
+/* Write a JavaScript function that returns array elements larger than a number.*/
+
+// let arr = [1,22,4,55,4,3,53,12,34,11,66];
+// let num = 30;
+// function getElement(arr,num){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// getElement(arr,num);
+
+let arr =[22,32,43,1929,2323,22,23,4,583,33,45,553,56,30,12];
+let num = 50;
+function getNum(arr,num){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>num){
+            console.log(arr[i]);
+        }
+    }
+}
+getNum(arr,num);
