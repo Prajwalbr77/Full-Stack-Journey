44288572b5746dae941f0a250a30aa4dea324075
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-06-30 12:40:41 +0530

    Add files via upload

---
+++ Javascript-P6/AssignQs2.js
@@ -0,0 +1,14 @@
+/*Write a JavaScript function to extract unique characters from a string. Example : str=“abcdabcdefgggh” ans=“abcdefgh */
+
+let str = "aabbccddaaiaojfsaflksjflkfjdjfiejfsdj";
+function uniqueChar(str){
+    let ans = " ";
+    for(let i=0;i<str.length;i++){
+        let currentChar = str[i];
+        if(ans.indexOf(currentChar)==-1){
+            ans +=currentChar;
+        }
+    }
+    return ans;
+}
+uniqueChar(str);
