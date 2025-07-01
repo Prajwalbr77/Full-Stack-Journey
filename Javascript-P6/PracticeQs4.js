bbeb3da9b7c3010284097279b06f422c40617997
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-01 21:31:36 +0530

    Add files via upload

---
+++ Javascript-P6/PracticeQs4.js
@@ -0,0 +1,11 @@
+function printTable(n){
+for(let i=n;i<=n*10;i=i+n){
+   console.log(i);
+}
+}
+
+function printTable1(n){
+   for(let i=n;i<=n;i++){
+      console.log(`${n} x ${i} = ${n*i}`);
+   }
+}
