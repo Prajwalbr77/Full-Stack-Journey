0bb6a4f6b95bb8e569ef1d812defe7c3ba927de0
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-01 21:28:32 +0530

    Add files via upload

---
+++ Javascript-P6/HigherOrdReturn.js
@@ -0,0 +1,38 @@
+function oddEvennFactory(request){
+    if(request=="odd"){
+      return function(n){
+    console.log(!(n%2==0));
+}
+    }
+    else if(request =="even"){
+      return function(n){
+    console.log(n%2==0);
+      }
+    }
+    else{
+        console.log("Wrong request!");
+    }
+}
+let req = "odd";
+
+
+
+function oddEvenFactory(request){
+  if(request == "odd"){
+    let odd = function(n){
+      console.log(!(n%2==0));
+    }
+    return odd;
+  }
+  else if(request =="even"){
+    let even = function(n){
+      console.log(n%2==0);
+    }
+    return even;
+  }
+  else{
+    console.log("Wrong request!");
+  }
+
+}
+let request = "even";
