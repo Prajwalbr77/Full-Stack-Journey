5c0b88ea4ca40d5ca304448409e89d75d5b4a9b4
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-05 11:20:51 +0530

    Update forloopp.js

--- Javascript-P4/forloopp.js
+++ Javascript-P4/forloopp.js
@@ -1,8 +1,8 @@
-// let num = Number(prompt("Enter the number:"));
-// for(let i=0;i<=num;i++){
-//     console.log(i);
-// }
-// let num = Number(prompt("Enter the number:"));
-// for(let i=num;i>=1;i--){
-//     console.log(i)
-// }
+let num = Number(prompt("Enter the number:"));
+for(let i=0;i<=num;i++){
+    console.log(i);
+}
+let num = Number(prompt("Enter the number:"));
+for(let i=num;i>=1;i--){
+    console.log(i)
+}
