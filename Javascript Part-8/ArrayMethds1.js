8d896b326c7753382155fe70c90b5326d84a6e27
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-17 18:17:48 +0530

    Add files via upload

---
+++ Javascript Part-8/ArrayMethds1.js
@@ -0,0 +1,14 @@
+let arr = [{
+    name : "Prajwal B R",
+    marks : 95,
+},{
+        name : "Prathista",
+    marks : 99,
+},{
+        name : "Devil Danaiel",
+    marks : 95,
+}
+];
+arr.forEach((student)=>{
+    console.log(student.marks);
+})
