346b684ebf4f1fc82640830aaee16959f803becc
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-17 18:28:50 +0530

    Add files via upload

---
+++ Javascript Part-8/destructuringObjs.js
@@ -0,0 +1,8 @@
+const student = {
+    name:"prajwal B R",
+    age : 21,
+    username : "_prajwalbr",
+    email : "prajwalprajwalbr@gmail.com"
+};
+const{username:user,email:gmail}=student;
+console.log(gmail);
