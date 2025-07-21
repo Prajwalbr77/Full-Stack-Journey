5c1f4478d462998077209918f1cfdf2799ab2a09
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-21 22:04:55 +0530

    Update AssignQs1.js

--- Javascript Part-9/AssignQs1.js
+++ Javascript Part-9/AssignQs1.js
@@ -22,3 +22,5 @@ document.querySelector("body").append(h1);
 let p = document.createElement("p");
 p.innerHTML = "Apna College <b>Delta</b> Practice";
 document.querySelector("body").append(p);
+
+console.log("hello");
