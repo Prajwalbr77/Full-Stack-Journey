9b968b869a562bc9439e8e8705abce345f301ded
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-07-27 12:02:54 +0530

    Update GoodString.js

--- Javascript/GoodString.js
+++ Javascript/GoodString.js
@@ -1,6 +1,10 @@
 let str = "appriciate";
 if((str[0]==='a')&&(str.length>3)){
     console.log("It is a good string!");
-}else{
+}else if{
     console.log("It is not a good string!");
 }
+else if{
+    console.log("Its exceeding");
+}
+
