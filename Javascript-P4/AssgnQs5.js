482ccabf7dd6328f8a23117506b3d1e3169ed7ef
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-08-02 21:58:39 +0530

    Update AssgnQs5.js

--- Javascript-P4/AssgnQs5.js
+++ Javascript-P4/AssgnQs5.js
@@ -6,3 +6,10 @@ for(let i=0; i<=arr.length;i++){
     }
 }
 console.log(largest);
+let arr = [2,3,4,3,5,3,24,];
+let largest =0;
+for(let i=0;i<arr.length;i++){
+    if(largest < arr[i]){
+        largest = arr[i];
+    }
+}
