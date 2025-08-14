838c7d92df043b68e55e42f4b266ed83a0c84f9c
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-08-14 21:20:42 +0530

    Update AssignQs5.js

--- Javascript-P6/AssignQs5.js
+++ Javascript-P6/AssignQs5.js
@@ -11,6 +11,5 @@ function generateRandom(start,end){
 let start = 100;
 let end = 200;
 function generateRandom(start,end){
-    let diff = end - start;
-    return Math.floor(Math.random()*diff)+start;
 }
+
