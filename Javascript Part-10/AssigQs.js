b26570edcfa1d1c5562349eeb58424d21a8cdc71
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-08-24 12:04:23 +0530

    Update AssigQs.js

--- Javascript Part-10/AssigQs.js
+++ Javascript Part-10/AssigQs.js
@@ -13,4 +13,3 @@ inp.addEventListener('input',function(){
 let inp = document.querySelector('input');
 let h3 = document.querySelector('h4');
 inp.addEventListner('input',function(){
-    const filtered = inp.value.replace(h3);
