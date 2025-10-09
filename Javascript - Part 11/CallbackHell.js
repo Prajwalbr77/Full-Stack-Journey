dfcd9b214557964d47c90b432bf49c4828ee0351
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-10-09 07:49:40 +0530

    Update CallbackHell.js

--- Javascript - Part 11/CallbackHell.js
+++ Javascript - Part 11/CallbackHell.js
@@ -44,4 +44,14 @@ changeColor("red",1000)
 //callback nesting => callback hell
 
 
+function changeColor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        h1.style.color=color;
+        resolve("color changed!");
+     },delay);
+    });
+}
+
+
 
