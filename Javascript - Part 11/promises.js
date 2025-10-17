03c7da474621b7d2064f4baaa5529da183253d83
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-10-17 19:35:54 +0530

    Update promises.js

--- Javascript - Part 11/promises.js
+++ Javascript - Part 11/promises.js
@@ -16,3 +16,23 @@ savetoDb("PRAJWAL BR")
 .catch(()=>{
     console.log("promise was rejected");
 })
+
+
+
+
+
+
+
+
+
+
+function savetoDb(data){
+   return new Promise((resolve, reject)=>{
+    let internetSpeed = Math.floor(Math.random()*10) +1;
+    if(internetSpeed > 4){
+        resolve("success: data was saved");
+    } else {
+        reject("failure: weak connection");
+    }
+   });
+}
