5dd11dd640163199283224803fec2e0cf8d6fd76
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-10-20 13:34:27 +0530

    Update CallbackHell.js

--- Javascript - Part 11/CallbackHell.js
+++ Javascript - Part 11/CallbackHell.js
@@ -54,8 +54,8 @@ changeColor("red",1000)
      changeColor("green",1000,()=>{
          changeColor("blue",1000,()=>{
              changeColor("yellow",1000,()=>{
-                 changeColor("pink");
              });
          });
      });
  });
+
