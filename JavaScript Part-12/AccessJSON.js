07557e16c32754c67bc9338d84b6c3d79ccd3fb4
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-11-03 14:24:50 +0530

    Update AccessJSON.js

--- JavaScript Part-12/AccessJSON.js
+++ JavaScript Part-12/AccessJSON.js
@@ -16,8 +16,5 @@ let jesResp = '{"fact":"According to a Gallup poll, most American pet owners obt
 let validRes = JSON.parse(jesResp);
 console.log(validRes.fact);
 
-let student = {
-    name : "Prajwal B R",
-    marks: 95
+let student = {};
 
-};
