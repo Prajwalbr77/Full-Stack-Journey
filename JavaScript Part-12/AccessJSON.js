7eaf618af9e90afd44b00946fe481e21adf0fd8a
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-11-03 14:25:17 +0530

    Update AccessJSON.js

--- JavaScript Part-12/AccessJSON.js
+++ JavaScript Part-12/AccessJSON.js
@@ -13,7 +13,4 @@ let student = {
 
 let jesResp = '{"fact":"According to a Gallup poll, most American pet owners obtain their cats by adopting strays.","length":90}';
 
-let validRes = JSON.parse(jesResp);
-console.log(validRes.fact);
-
 
