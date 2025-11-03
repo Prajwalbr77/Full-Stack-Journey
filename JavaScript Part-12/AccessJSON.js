37030a3872fa31aef66e1303088b73482c012e0d
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-11-03 14:24:30 +0530

    Update AccessJSON.js

--- JavaScript Part-12/AccessJSON.js
+++ JavaScript Part-12/AccessJSON.js
@@ -10,3 +10,14 @@ let student = {
 };
 
 
+
+let jesResp = '{"fact":"According to a Gallup poll, most American pet owners obtain their cats by adopting strays.","length":90}';
+
+let validRes = JSON.parse(jesResp);
+console.log(validRes.fact);
+
+let student = {
+    name : "Prajwal B R",
+    marks: 95
+
+};
