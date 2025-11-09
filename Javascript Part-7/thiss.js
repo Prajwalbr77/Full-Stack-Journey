7c7c6befd8d6879d6dd428368d605d8f2b947841
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2025-11-09 21:51:49 +0530

    Update thiss.js

--- Javascript Part-7/thiss.js
+++ Javascript Part-7/thiss.js
@@ -8,4 +8,20 @@ const student ={
         let avvg = (this.age + this.coding + this.knowledge) /3;
         console.log(avvg);
     }
+
+}
+
+
+
+const student ={
+    name : "Prajwal",
+    age : 21,
+    coding : 78,
+    knowledge : 80,
+    ability : 95,
+    getAvg(){
+        let avvg = (this.age + this.coding + this.knowledge) /3;
+        console.log(avvg);
+    }
+
 }
