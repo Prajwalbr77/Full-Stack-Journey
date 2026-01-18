ec96a35e59ff9a0dca634864eedc9385927f2f57
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2026-01-18 11:20:09 +0530

    Refactor getJokes function to remove duplication
    Removed duplicate getJokes function and cleaned up code.

--- JavaScript Part 13/headerss.js
+++ JavaScript Part 13/headerss.js
@@ -20,9 +20,8 @@ const url = "https://icanhazdadjoke.com/";
 async function getJokes() {
     try {
         const config = {headers: {Accept: "application/json"}};
-     let res = await axios.get(url,config);
-     console.log(res.data);
     }
 }
+
 
 
