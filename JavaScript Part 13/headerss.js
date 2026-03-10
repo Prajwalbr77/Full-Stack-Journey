1d3bae5d58fa952b4b07f8e4a58e96074dd16209
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2026-03-10 12:46:01 +0530

    Update headerss.js

--- JavaScript Part 13/headerss.js
+++ JavaScript Part 13/headerss.js
@@ -10,4 +10,16 @@ async function getJokes() {
   console.log(err);
     }
 
+
+
+    try {
+        const config = {headers: {Accept: "application/json"}};
+     let res = await axios.get(url,config);
+     console.log(res.data);
+    }
+    catch(err){
+  console.log(err);
+    }
+
 }
+
