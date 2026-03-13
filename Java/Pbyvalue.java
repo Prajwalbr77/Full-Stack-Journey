ba2a47641b424e0976c415cff080c2db500b2ada
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2026-03-13 18:22:46 +0530

    Add files via upload

---
+++ Java/Pbyvalue.java
@@ -0,0 +1,12 @@
+
+public class Pbyvalue {
+    public static void main(String args[]) {
+	  int 	a =1000;
+	  int b = a;
+	  System.out.println(a);
+	  System.out.println(b);
+	  b = 2000;
+	  System.out.println(a);
+	  System.out.println(b);
+	}
+}
