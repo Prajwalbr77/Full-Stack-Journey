e8083bd4b45c459f191f24e04c8c17221b1b7494
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2026-03-17 22:49:41 +0530

    Refactor Square class to remove duplicate code
    Removed duplicate code and cleaned up formatting.

--- Java/square.java
+++ Java/square.java
@@ -18,11 +18,6 @@ class Square {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        
     }
 }
