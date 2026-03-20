cc57df14b8d6e1f0eeb4f12082c5c79ff51645a4
PRAJWAL B R <141422513+Prajwalbr77@users.noreply.github.com>
2026-03-20 14:01:06 +0530

    Update square.java

--- Java/square.java
+++ Java/square.java
@@ -21,11 +21,6 @@ class Square {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
 
